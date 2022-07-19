6276678938c4a136154f36497d7cbf8eaf6de4cd
Kevin <kevin.castro.chavez0@gmail.com>
2022-07-19 00:40:47 -0600

    C# game for the 210 class

---
+++ Assets/Scripts/Director.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Director : MonoBehaviour
+{
+    public  Text scoreText;
+    private Blade blade;
+    public Image gameoverImg;
+    private Spawner spawner; 
+
+    private int score;
+
+    private void Awake() 
+    {
+        blade = FindObjectOfType<Blade>();
+        spawner = FindObjectOfType<Spawner>();
+    }
+
+    private void Start()
+    {
+        NewGame();
+    }
+
+    private void NewGame()
+    {
+        Time.timeScale = 1f;
+
+        blade.enabled = true;
+        spawner.enabled = true;
+        
+        score = 0;
+        scoreText.text = score.ToString();
+
+        ClearScene();
+    }
+
+    private void ClearScene()
+    {
+        Fruit[] fruits = FindObjectsOfType<Fruit>();
+
+        foreach (Fruit fruit in fruits)
+        {
+            Destroy(fruit.gameObject);
+        }
+
+        Bomb[] bombs = FindObjectsOfType<Bomb>();
+
+        foreach (Bomb bomb in bombs)
+        {
+            Destroy(bomb.gameObject);
+        }
+    }
+
+    public void IncreaseScore()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+
+    public void Explode()
+    {
+        blade.enabled = false;
+        spawner.enabled = false;
+
+        StartCoroutine(ExplodeSequence());
+    }
+
+    private IEnumerator ExplodeSequence()
+    {
+        float elapsed = 0f;
+        float duration = 0.5f;
+
+        while (elapsed < duration)
+        {
+            float t = Mathf.Clamp01(elapsed / duration);
+            gameoverImg.color = Color.Lerp(Color.clear, Color.white, t);
+
+            Time.timeScale = 1f - t;
+            elapsed += Time.unscaledDeltaTime;
+            
+            yield return null;
+        }
+
+        yield return new WaitForSecondsRealtime(1f);
+
+        NewGame();
+
+        elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            float t = Mathf.Clamp01(elapsed / duration);
+            gameoverImg.color = Color.Lerp(Color.white, Color.clear, t);
+
+            Time.timeScale = 1f - t;
+            elapsed += Time.unscaledDeltaTime;
+            
+            yield return null;
+        }
+    }
+}
